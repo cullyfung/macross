884e1c6f2b524c8b629b19ae341e877c3e29a0fd
cullyfung <cullyfung@gmail.com>
2023-03-16 21:36:00 +0800

    chore: init

---
+++ play/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MIcon from "@macross/components/icon";
+import { AccessibilityOutline } from "@vicons/ionicons5";
+</script>
+
+<template>
+  <div>
+    <MIcon>
+      <AccessibilityOutline></AccessibilityOutline>
+    </MIcon>
+  </div>
+</template>
---
+++ play/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import '@macross/theme-chalk/src/index.scss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
